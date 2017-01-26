0745dd6dc372900b10602927bc8fbf99a1c64e09
Nauval Atmaja <nauval@tripvisto.com>
2017-01-26 11:51:24 +0700

    Bookshelf models for testing
    Why:
    Need bookshelf models to test the plugin
    
    This change addresses the need by:
    Creating the models based using blog scenario:
    Author, Post and Comment

--- src/plugin.spec.js
+++ src/plugin.spec.js
@@ -1,4 +1,6 @@
 import { expect } from 'chai'; // eslint-disable-line
+import knex from 'knex'; // eslint-disable-line
+import Bookshelf from 'bookshelf'; // eslint-disable-line
 import plugin from './plugin';
 
 // Test data set:
@@ -7,6 +9,45 @@ import plugin from './plugin';
 // - comments { id, author_id, post_id, comment }
 
 describe('plugin', () => {
+  Bookshelf.plugin('registry');
+
+  const Author = Bookshelf.Model.extend({
+    tableName: 'users',
+    defaults: {
+      name: 'Anonymous',
+    },
+    posts() {
+      return this.hasMany('Post');
+    },
+    comments() {
+      return this.hasMany('Comment');
+    },
+  });
+  Bookshelf.model('Author', Author);
+
+  const Post = Bookshelf.Model.extend({
+    tableName: 'posts',
+    defaults: {
+      title: 'foo bar',
+      post: 'Lorem ipsum dolor sit amet',
+    },
+    author() {
+      return this.belongsTo('Author');
+    },
+  });
+  Bookshelf.model('Post', Post);
+
+  const Comment = Bookshelf.Model.extend({
+    tableName: 'comments',
+    defaults: {
+      comment: 'Hi there',
+    },
+    author() {
+      return this.belongsTo('Author');
+    },
+  });
+  Bookshelf.model('Comment', Comment);
+
   describe('filter', () => {
     describe('posts?filter[title]=foo', () => {
       it('returns post with title foo', (done) => {
